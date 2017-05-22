84d893116cf75e3c94277bc8e98f785c06e1e09b
The Dark Groove <catrielcruz1995@gmail.com>
2017-05-22 15:25:55 -0500

    Initial Commit

---
+++ src/main/java/com/sismat/SisMatApplication.java
@@ -0,0 +1,12 @@
+package com.sismat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisMatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisMatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sismat/SisMatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sismat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SisMatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
